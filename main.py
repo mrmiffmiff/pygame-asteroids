de68da827e01fafaa9b3b23ce4f4e2305b29e7c2
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-11-13 15:20:55 -0800

    player movement

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        player1.update(dt)
         screen.fill("black")
         player1.draw(screen)
         pygame.display.flip()
