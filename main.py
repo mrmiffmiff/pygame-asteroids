3d4d204cc7991bc3c617c6461702a0c0154d8bf8
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-11-13 14:50:33 -0800

    player object in center of the screen

--- main.py
+++ main.py
@@ -5,22 +5,31 @@ import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
     while True:
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
         time_passed = clock.tick(60)
         dt = time_passed / 1000
 
