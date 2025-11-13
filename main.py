4d3fff928b1e6e38cd48d7be6bb0b1cdd3bc3600
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-11-13 14:35:08 -0800

    changes to account for logging and also adding intro text back

--- main.py
+++ main.py
@@ -3,20 +3,26 @@
 # throughout this file
 import pygame
 
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
     while True:
+        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-        dt = clock.tick(60) / 1000
+        time_passed = clock.tick(60)
+        dt = time_passed / 1000
 
 
 if __name__ == "__main__":
